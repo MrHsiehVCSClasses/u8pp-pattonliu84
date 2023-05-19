64bc5b39ed84c9426a789445a536a12eda2a621a
pattonliu84 <113130211+pattonliu84@users.noreply.github.com>
2023-05-19 05:48:16 -0700

    idk i added bubble sort

--- src/main/java/u8pp/SearchSort.java
+++ src/main/java/u8pp/SearchSort.java
@@ -145,6 +145,26 @@ public class SearchSort {
         return done;
     }
 
+ public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped in the inner loop, the array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
  
     public static int compare(Student o1, Student o2) {
         if (o1.getYear() != o2.getYear()) {
